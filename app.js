533b04c85b122e0a7e1463b074be2246500f15e6
Ajitesh Saxena <18ucs137@lnmiit.ac.in>
2021-11-29 02:19:16 +0530

    upload working properly

--- app.js
+++ app.js
@@ -4,34 +4,54 @@ const sequelize = require('./database');
 const path = require('path');
 const InterviewDetails = require('./interview_details');
 const PORT = (process.env.PORT||8000);
+const multer = require('multer');
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public");
+    },
+    filename: (req, file, cb) => {
+      const ext = file.mimetype.split("/")[1];
+      const name = file.originalname.split(".")[0];
+      cb(null, 'resumes/'+name+Date.now()+"."+ext);
+    },
+});
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.split("/")[1] === "pdf") {
+      cb(null, true);
+    } else {
+      cb(new Error("Not a PDF File!!"), false);
+    }
+  };
+
+  const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+  });
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) => {
-    console.log('index loaded');
     res.sendFile(__dirname + '/public/index.html');
-})
+});
 
 app.get('/create',(req,res) => {
-    console.log('Create loaded');
     res.sendFile(__dirname + '/public/create.html');
 });
 
-app.post('/create',(req,res) => {
-    console.log();
-    console.log(req.body);
+app.post('/create',upload.single('candidateResume'),(req,res) => {
     InterviewDetails.create({
         interviewer: req.body.interviewerName,
         interviewer_email: req.body.interviewerEmail,
         candidate: req.body.candidateName,
         candidate_email: req.body.candidateEmail,
         startTime : req.body.startTime,
-        endTime : req.body.endTime
+        endTime : req.body.endTime,
+        resume: req.file.path
     }).then((data) => {
-        console.log(data);
-        res.send(data);
+        res.sendFile(__dirname+'\\'+data.resume);
     });
 });
 
