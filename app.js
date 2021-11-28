ef17730c028ae00eecee4c43541e395cb93f6868
Ajitesh Saxena <18ucs137@lnmiit.ac.in>
2021-11-29 00:20:35 +0530

    first-draft

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./database');
+const path = require('path');
 const InterviewDetails = require('./interview_details');
-
+const ejs = require('ejs');
 const PORT = (process.env.PORT||8000);
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res) => {
+    console.log('Form loaded');
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 sequelize.sync({force: true})
     .then(() => {
         console.log('Model was synchronized');
-        app.listen(PORT,(req,res) => {
-            console.log('server stated at port '+PORT)
+        app.listen(PORT,() => {
+            console.log('server started at port '+PORT)
         })
     });
--- interview_details.js
+++ interview_details.js
@@ -2,7 +2,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('./database');
 let InterviewDetails = sequelize.define('InterviewDetails', {
     interviewer: DataTypes.STRING,
-    participants: DataTypes.STRING,
+    interviewer_email: DataTypes.STRING,
+    candidate: DataTypes.STRING,
+    candidate_email: DataTypes.STRING,
     startTime: DataTypes.TIME,
     endTime: DataTypes.TIME,
     resume: DataTypes.STRING
