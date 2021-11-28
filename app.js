abb0ba7d742769d014ddcf69c65e9832b5821b2d
Ajitesh Saxena <18ucs137@lnmiit.ac.in>
2021-11-29 01:26:33 +0530

    create interview added

--- app.js
+++ app.js
@@ -3,17 +3,37 @@ const app = express();
 const sequelize = require('./database');
 const path = require('path');
 const InterviewDetails = require('./interview_details');
-const ejs = require('ejs');
 const PORT = (process.env.PORT||8000);
 
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) => {
-    console.log('Form loaded');
+    console.log('index loaded');
     res.sendFile(__dirname + '/public/index.html');
 })
+
+app.get('/create',(req,res) => {
+    console.log('Create loaded');
+    res.sendFile(__dirname + '/public/create.html');
+});
+
+app.post('/create',(req,res) => {
+    console.log();
+    console.log(req.body);
+    InterviewDetails.create({
+        interviewer: req.body.interviewerName,
+        interviewer_email: req.body.interviewerEmail,
+        candidate: req.body.candidateName,
+        candidate_email: req.body.candidateEmail,
+        startTime : req.body.startTime,
+        endTime : req.body.endTime
+    }).then((data) => {
+        console.log(data);
+        res.send(data);
+    });
+});
 
 sequelize.sync({force: true})
     .then(() => {
